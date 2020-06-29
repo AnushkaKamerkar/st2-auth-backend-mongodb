10e2c3633e2125fb10d357d25c44ae2d432d7926
AnushkaKamerkar <anushka@coditation.com>
2020-06-29 11:49:55 +0530

    code refactor

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Licensed to the StackStorm, Inc ('StackStorm') under one or more
+# Licensed to the Coditation, Inc ('Coditation') under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
@@ -34,12 +34,12 @@ install_reqs, dep_links = fetch_requirements(REQUIREMENTS_FILE)
 setup(
     name='st2-auth-backend-mongodb',
     version=version,
-    description='StackStorm authentication backend which reads credentials from a MongoDB collection.',
-    author='StackStorm, Inc.',
-    author_email='info@stackstorm.com',
-    url='https://github.com/StackStorm/st2-auth-backend-mongodb',
+    description='Coditation authentication backend which reads credentials from a MongoDB collection.',
+    author='Coditation, Inc.',
+    author_email='info@coditation.com',
+    url='https://github.com/Coditation/st2-auth-backend-mongodb',
     license='Apache License (2.0)',
-    download_url='https://github.com/StackStorm/st2-auth-backend-mongodb/tarball/master',
+    download_url='https://github.com/Coditation/st2-auth-backend-mongodb/tarball/master',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
